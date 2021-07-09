32d1b998b3ac6d558699d909f4c80f565483e4c4
Andrey Mishchenko <buryonok@gmail.com>
2021-07-09 20:56:09 +0300

    Add bath update to updater.js

--- src/updater.js
+++ src/updater.js
@@ -6,6 +6,24 @@ import loadRss from './loader.js';
 const updateRss = (state) => {
   const urls = state.feeds
     .map(({ url }) => url);
+  const urlsChunks = _.chunk(urls, 10);
+  urlsChunks.forEach((chunkUrls) => {
+    const promises = chunkUrls
+      .map(loadRss);
+    Promise.all(promises)
+      .then((results) => {
+        const posts = results
+          .flatMap((result) => result.posts);
+        const allPosts = _.union(posts, state.posts);
+        const newPosts = _.differenceBy(allPosts, state.posts, 'postLink');
+        if (newPosts.length > 0) {
+          state.posts = [...newPosts, ...state.posts];
+        }
+      })
+      .finally(() => {
+        setTimeout(() => updateRss(state), 5000);
+      });
+  });
   const promises = urls
     .map(loadRss);
   Promise.all(promises)
